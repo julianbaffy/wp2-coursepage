15da524a95f78839e962dc89c1f4279b7a764818
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-06-17 13:35:09 +0200

    Implement Tabs for LinkGrids on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import LinkGrid from "$lib/Components/LinkGrid.svelte";
+	import { Tabs } from '@skeletonlabs/skeleton-svelte';
+
+  let group = $state('1');
 </script>
 
 <svelte:head>
@@ -17,7 +20,18 @@
 	</h2>
 </section>
 
-<LinkGrid courseID="2" />
+<Tabs value={group} onValueChange={(e) => (group = e.value)} fluid>
+  {#snippet list()}
+    <Tabs.Control value="1">&lt; IF1 BEH &#47; &gt;</Tabs.Control>
+    <Tabs.Control value="2">IF2 BAF</Tabs.Control>
+  {/snippet}
+  {#snippet content()}
+    <Tabs.Panel value="1"><LinkGrid courseID="1" /></Tabs.Panel>
+    <Tabs.Panel value="2"><LinkGrid courseID="2" /></Tabs.Panel>
+  {/snippet}
+</Tabs>
+
+
 
 <style>
 	section {
