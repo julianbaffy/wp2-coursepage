b88cfab0ddb22876d9f14120d41f74eab21c4c3a
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-06-20 14:07:03 +0200

    Create CourseTabs.svelte

---
+++ src/lib/Components/CourseTabs.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import LinkGrid from "./LinkGrid.svelte";
+
+    let {courses, links, startPosition=0 } = $props();
+
+    let currentPosition=$state(startPosition);
+
+    function openTab(id){
+        currentPosition=id;
+        var i;
+        var i;
+        var x = document.getElementsByClassName("tab-item");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(id).style.display = "block"; 
+    }
+</script>
+
+
+<div class="container w-full">
+    <div class="tab-controls w-full flex justify-between">
+        {#each data.courses as course}
+		    <button class="" >IF{course.courseID} {course.teacher}</button>
+	    {/each}
+    </div>
+    <div class="tab-content w-full">
+        {#each courses as course, i}
+            <div id="{course.courseID}" class="tab-item">
+                <LinkGrid courseID="{course.courseID}" links={data.links} />
+            </div>
+        {/each}
+    </div>
+</div>
+
+
+
+<style>
+
+</style>
