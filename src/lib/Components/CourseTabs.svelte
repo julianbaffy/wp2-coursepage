0f8efd38975cb2537f66be5acd370bce4c51d926
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-06-20 15:13:30 +0200

    Update CourseTabs.svelte

--- src/lib/Components/CourseTabs.svelte
+++ src/lib/Components/CourseTabs.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import LinkGrid from "./LinkGrid.svelte";
+    import { onMount } from "svelte";
 
     let {courses, links, startPosition="1" } = $props();
 
@@ -21,8 +22,7 @@
 <div class="tab-container w-full mt-8">
     <div class="tab-controls w-full flex border-gray-300 relative pb-4">
 		{#each courses as course}
-			<div id={course.courseID}
-            class={`tab-item ${currentPosition === course.courseID ? 'block' : 'hidden'}`}>
+			<div class="button-container w-full text-center py-2 border-b-1 {currentPosition === course.courseID ? 'border-black opacity-100' : 'border-gray-300 opacity-50 hover:opacity-100'} transition-all duration-200">
                 <button
                     onclick={() => openTab(course.courseID)}
                     class="w-full text-center py-1 hover:bg-white/80 transition-all duration-200"
