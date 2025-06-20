8a2c68f60d453b0e5190d204a98c3d025990f310
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-06-20 15:11:40 +0200

    Update CourseTabs Syntax

--- src/lib/Components/CourseTabs.svelte
+++ src/lib/Components/CourseTabs.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import LinkGrid from "./LinkGrid.svelte";
-    import { onMount } from "svelte";
 
     let {courses, links, startPosition="1" } = $props();
 
@@ -22,7 +21,8 @@
 <div class="tab-container w-full mt-8">
     <div class="tab-controls w-full flex border-gray-300 relative pb-4">
 		{#each courses as course}
-			<div class="button-container w-full text-center py-2 border-b-1 {currentPosition === course.courseID ? 'border-black opacity-100' : 'border-gray-300 opacity-50 hover:opacity-100'} transition-all duration-200">
+			<div id={course.courseID}
+            class={`tab-item ${currentPosition === course.courseID ? 'block' : 'hidden'}`}>
                 <button
                     onclick={() => openTab(course.courseID)}
                     class="w-full text-center py-1 hover:bg-white/80 transition-all duration-200"
@@ -34,8 +34,8 @@
 	</div>
     <div class="tab-content w-full">
         {#each courses as course, i}
-            <div id="{course.courseID}" class="tab-item {currentPosition === course.courseID ? 'block' : 'hidden'}">
-                <LinkGrid courseID="{course.courseID}" links={links} />
+            <div id={course.courseID} class="tab-item {currentPosition === course.courseID ? 'block' : 'hidden'}">
+                <LinkGrid courseID={course.courseID} links={links} />
             </div>
         {/each}
     </div>
