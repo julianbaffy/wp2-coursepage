5ef87cbf6393254e9741d6510e08546c05002498
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-06-24 13:50:33 +0200

    Add sticky position and backdrop blur to tab controls

--- src/lib/Components/CourseTabs.svelte
+++ src/lib/Components/CourseTabs.svelte
@@ -2,6 +2,7 @@
     
     import LinkGrid from "./LinkGrid.svelte";
     import TabsButton from "./TabsButton.svelte";
+    import { onMount } from "svelte";
 
     let {courses, links, startPosition="1" } = $props();
 
@@ -20,13 +21,35 @@
             el.style.display = "block";
         }
     }
+
+    // Intersection-Observer for tab-controls sentinel
+
+    let sentinel: HTMLDivElement;
+	let isSticky = $state(false);
+
+	// Intersection Observer einrichten
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				isSticky = !entry.isIntersecting;
+			},
+			{ rootMargin: '0px 0px 0px 0px', threshold: [1] }
+		);
+
+		if (sentinel) {
+			observer.observe(sentinel);
+		}
+
+		return () => observer.disconnect();
+	});
 </script>
 
 
 <div class="tab-container w-full mt-8">
-    <div class="tab-controls w-full flex relative pb-4">
+    <div bind:this={sentinel} class="h-1"></div>
+    <div class:stuck={isSticky} class="tab-controls sticky top-14 w-full flex relative pb-4 z-20">
 		{#each courses as course}
-			<div class="button-container w-full text-center py-2 border-gray-400 {currentPosition === course.courseID ? 'border-x border-t rounded-t-[8px] bg-gradient-to-b from-white to-transparent' : 'border-b-1 hover:bg-gradient-to-t hover:from-white hover:to-transparent'} transition-opacity duration-200">
+			<div class="button-container w-full text-center py-4 border-gray-400 {currentPosition === course.courseID ? 'border-x border-t rounded-t-[8px] bg-gradient-to-b from-white to-transparent' : 'border-b-1 hover:bg-gradient-to-t hover:from-white hover:to-transparent'} transition-opacity duration-200">
                 <TabsButton courseID={course.courseID} courseTeacher={course.teacher} isActive={currentPosition === course.courseID} openTab={openTab} />
             </div>
 		{/each}
@@ -39,3 +62,24 @@
         {/each}
     </div>
 </div>
+
+<style>
+    .tab-controls::before {
+	content: "";
+	position: absolute;
+	inset: 0;
+    left: -4em;
+    right: -4em;
+    bottom: -0.6em;
+	backdrop-filter: blur(30px);
+	-webkit-backdrop-filter: blur(30px);
+	mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+	-webkit-mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+	pointer-events: none;
+	z-index: -1;
+}
+
+    .tab-controls.stuck::before{
+        top: -3.5em;
+    }
+</style>
