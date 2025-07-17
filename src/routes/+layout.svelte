cb9b1d48fd197daa2af5351c18af9248fbdaca95
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-07-18 00:26:43 +0200

    Add scrollTo top: 0 to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import Header from './Header.svelte';
 	import '../app.css';
+	import { afterNavigate } from '$app/navigation';
+
+	afterNavigate(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+	});
 	
 	let { children } = $props();
 </script>
