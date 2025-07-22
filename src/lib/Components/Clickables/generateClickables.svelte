2825047e2f345f911f8ca4c599b4edd83d144bcb
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-07-22 13:37:00 +0200

    Create generateClickables.svelte

---
+++ src/lib/Components/Clickables/generateClickables.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Clickable from "./clickable.svelte";
+
+   let width = $state(0);
+   let height =$state(0);
+
+	function updateSize() {
+    	width = window.innerWidth;
+        height = window.innerHeight;
+  	}
+    
+</script>
+<div class="clickable-container">
+
+</div>
