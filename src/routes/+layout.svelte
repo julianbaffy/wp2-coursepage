35dae6af6efb1cdff88862afbc551c3d825b0f2d
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-08-17 09:53:39 +0200

    change vh to vw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,7 @@
 		display: flex;
 		flex-grow: 1;
 		flex-direction: column;
-		max-width: 100vh;
+		max-width: 100vw;
 	}
 
 	main {
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -46,7 +46,7 @@
 		top: 0;
 		left: 0;
 		right: 0;
-		width: 100vh;
+		width: 100vw;
 		z-index: 50;
 		padding-bottom: 1em;
 	}
